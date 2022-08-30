85df93b1f15ff71b0093421c6dd15847b55fb9a5
bijen <officiallybijen@gmail.com>
2022-08-30 09:04:32 +0545

    routing and path exception

--- resources/views/hi.blade.php
+++ resources/views/hi.blade.php
@@ -6,9 +6,9 @@
     </head>
     <body>
         <ul>
-            <li><a href="/blog">Blog 1</a></li>
-            <li><a href="/blog">Blog 2</a></li>
-            <li><a href="/blog">Blog 3</a></li>
+            <li><a href="/blog/firstblog">Blog 1</a></li>
+            <li><a href="/blog/secondblog">Blog 2</a></li>
+            <li><a href="/blog/thirdblog">Blog 3</a></li>
         </ul>
     </body>
 </html>
--- routes/web.php
+++ routes/web.php
@@ -17,6 +17,17 @@ Route::get('/hi', function () {
     return view('hi');
 });
 
-Route::get('/blog',function(){
-    return view('blog');
+Route::get('/blog/{which}',function($slug){
+    $path = __DIR__ ."/../resources/blogs/$slug.html";
+    
+    if(! file_exists($path)){
+        // abort(404);
+        return redirect("/hi");
+    }
+
+    $blog=file_get_contents($path);
+    
+    return view('blog',[
+        'blog'=>$blog
+    ]);
 });
