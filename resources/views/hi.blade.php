b53eb35e1086c6b7d42f7cd21c5edd7a1c006c76
bijen <officiallybijen@gmail.com>
2022-08-30 08:05:41 +0545

    css and js linking

---
+++ resources/views/hi.blade.php
@@ -0,0 +1,12 @@
+<html>
+    <head>
+        <title>practice</title>
+        <link rel="stylesheet" href="/app.css">
+        <script src="/app.js"></script>
+    </head>
+    <body>
+        <p>
+            hello 
+        </p>
+    </body>
+</html>
