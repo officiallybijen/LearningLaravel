20087d3b3693c382ff62b2ed7692333c79ab787a
bijen <officiallybijen@gmail.com>
2022-08-31 11:59:19 +0545

    end of ep 20

---
+++ resources/views/hi.blade.php
@@ -0,0 +1,13 @@
+<html>
+    <head>
+        <title>practice</title>
+        <link rel="stylesheet" href="/app.css">
+        <script src="/app.js"></script>
+    </head>
+    <body>
+        @foreach($blogs as $blog)
+          <a href="/blog/{{$blog->id}}">{{$blog->title}}</a>
+          <p>{!! $blog->body !!}</p>
+        @endforeach
+    </body>
+</html>
---
+++ routes/web.php
@@ -0,0 +1,83 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+use App\Models\Blog;
+use Spatie\YamlFrontMatter\YamlFrontMatter;
+use Illuminate\Support\Facades\File;
+
+
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/hi', function () {
+    // $doc=YamlFrontMatter::parseFile(
+    //     resource_path('blogs/firstblog.html')
+    // );
+
+
+    // $files=File::files(resource_path("blogs"));
+
+    $blogs=Blog::all();
+
+    // $blogs=array_map(function($file){
+    //     $document = YamlFrontMatter::parseFile($file);
+    //     return new Blog(
+    //         $document->title,
+    //         $document->slug,
+    //         $document->body()
+    //     );
+    // },$files);
+
+    // foreach($files as $file){
+    //     $document = YamlFrontMatter::parseFile($file);
+    //     $blog[] =new Blog(
+    //         $document->title,
+    //         $document->slug,
+    //         $document->body()
+    //     );
+    // }
+    // $blogs=Blog::all();
+    return view('hi',[
+        'blogs'=>$blogs
+    ]);
+});
+
+Route::get('/blog/{which}',function($id){
+
+    //find post by its slug and pass it to view called post
+
+    $blog=Blog::find($id);
+
+    return view('blog',[
+        'blog'=>$blog
+    ]);
+
+    // $path = __DIR__ ."/../resources/blogs/$slug.html";
+    
+
+    // if(! file_exists($path)){
+    //     // abort(404);
+    //     return redirect("/hi");
+    // }
+
+    // $blog = cache()->remember('blog.{which}',2,fn() => file_get_contents($path));
+
+    // $blog = cache()->remember('blog.{which}',2,function() use ($path){
+    //     return file_get_contents($path);
+    // });
+
+    // $blog=file_get_contents($path);
+    
+//     return view('blog',[
+//         'blog'=>$blog
+//     ]);
+})->where('which','[A-z_\-0-9]+');
